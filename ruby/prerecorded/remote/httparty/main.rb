e8bc74c1876fa97852be4e365fd8b257cf69edf8
Sandra Rodgers <sandra.rodgers@deepgram.com>
2024-03-01 15:58:30 -0600

    ruby code

---
+++ ruby/prerecorded/remote/httparty/main.rb
@@ -0,0 +1,22 @@
+require 'httparty'
+
+# Define the URL for the Deepgram API endpoint
+url = 'https://api.deepgram.com/v1/listen'
+
+# Define headers
+headers = {
+  'Accept' => 'application/json',
+  'Authorization' => 'Token YOUR_DEEPGRAM_API_KEY',
+  'Content-Type' => 'application/json'
+}
+
+# Define the request body in JSON format
+body = {
+  url: 'https://dpgr.am/spacewalk.wav'
+}.to_json
+
+# Make a POST request to the Deepgram API endpoint using HTTParty
+response = HTTParty.post(url, headers: headers, body: body)
+
+# Output the response
+puts response.body
